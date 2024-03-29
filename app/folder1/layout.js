dc89fb90c092facffd81e1ffd9880a00c9505b19
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2024-03-30 01:10:38 +0600

    'commited'

---
+++ app/folder1/layout.js
@@ -0,0 +1,10 @@
+
+
+export default function GalleryLayout({children,modal}) {
+  return (
+    <>
+      {modal}
+      {children}
+    </>
+  )
+}
---
+++ app/gallery/@modal/default.js
@@ -0,0 +1,5 @@
+
+
+export default function DefaultMordal() {
+  return null
+}
